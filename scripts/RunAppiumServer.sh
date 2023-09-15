7660500324aa797bc9740800738b5ca4bef40f3e
anamnizami <118906364+anamnizami@users.noreply.github.com>
2023-09-15 14:25:49 +0200

    Update RunAppiumServer.sh

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -ex
 npm install -g appium@next
-appium driver install xcuitest
 appium driver install espresso
-appium driver install flutter
 
 appium -v
 appium --log appium.log &>/dev/null &
